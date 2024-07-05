35ab689b9a103c6a2f75241434e60761d12e5b76
Naman Singh <naman.singh2402@gmail.com>
2024-07-05 13:37:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import io
 import torch
+from flask_cors import CORS
 import torch.nn as nn
 from torchvision import transforms
 from PIL import Image
@@ -30,7 +31,8 @@ transform = transforms.Compose([
 ])
 
 # Initialize Flask app
-app = Flask(__name__, static_folder='static') 
+app = Flask(__name__, static_folder='static')
+CORS(app) 
 # Route to serve the HTML file
 @app.route('/')
 def index():
